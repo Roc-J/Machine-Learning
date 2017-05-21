6b9d209d24a1621b64b2997b5739e3c001e4cd66
Roc-J <646481338@qq.com>
2017-05-21 14:42:06 +0800

    kNN arithmetic example

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -27,4 +27,61 @@ def classify(intX,dataSet,labels,k):
     sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
 
+# 将文本记录转换为Numpy的解析程序
+def file2matrix(filename):
+    fileReader = open(filename)
+    arrayLines = fileReader.readlines()
+    numberOfLines = len(arrayLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector =[]
+    index = 0
+    for line in arrayLines:
+        line = line.strip()
+        listFormLine = line.split('\t')
+        returnMat[index,:]= listFormLine[0:3]
+        classLabelVector.append(int(listFormLine[-1]))
+        index +=1
+    return returnMat,classLabelVector
+
+# 归一化特征值
+def autoNorm(dataSet):
+    # dataSet.min(0)中的参数0使得函数可以从列中选取最小值，而不是选取当前行的最小值
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet /tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+# 分类器针对约会网站的测试代码
+def datingClassTest():
+    rateTest = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*rateTest)
+    errorCount = 0
+    for i in range(numTestVecs):
+        classifierResult = classify(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],5)
+        print 'the classifier came back with %d，the real answer is %d' % (classifierResult,datingLabels[i])
+        if(classifierResult!=datingLabels[i]):
+            errorCount+=1
+    print 'the total error rate is : %f' %(errorCount/float(numTestVecs))
+
+
+# 使用算法，构建完整可用系统
+def classifyPerson():
+    resultList = ['不喜欢','一般喜欢','很喜欢']
+    percentTats = float(raw_input('percentage of time spent palying video games?'))
+    ffMiles = float(raw_input('frequent flier miles earned per year?'))
+    iceCream = float(raw_input('liters of ice cream consumed per yaer?'))
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    inputArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify((inputArr-minVals)/ranges,normMat,datingLabels,5)
+    print '你对这个人的评价是',resultList[classifierResult-1]
+
+# 下面的内容是手写识别系统
 
