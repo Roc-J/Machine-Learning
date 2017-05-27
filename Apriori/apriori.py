b8327e38cfbe3d975190c870bb7df23113d10627
Roc-J <646481338@qq.com>
2017-05-27 20:46:48 +0800

    apriori

--- Apriori/apriori.py
+++ Apriori/apriori.py
@@ -38,3 +38,28 @@ def scanD(dataSet,Ck,minSupport):
         supportData[key] = support
     return retList,supportData
 
+def aprioriGen(Lk, k): #creates Ck
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i+1, lenLk):
+            L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
+            L1.sort(); L2.sort()
+            if L1==L2: #if first k-2 elements are equal
+                retList.append(Lk[i] | Lk[j]) #set union
+    return retList
+
+def apriori(dataSet,minSupport=0.5):
+    C1 = createC1(dataSet)
+    D = map(set,dataSet)
+    L1,supportData = scanD(D,C1,minSupport)
+    L = [L1]
+    k=2
+    while(len(L[k-2])>0):
+        Ck = aprioriGen(L[k-2],k)
+        Lk,supK =scanD(D,Ck,minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k +=1
+    return L,supportData
+
